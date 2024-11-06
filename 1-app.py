163804763d90b878ee26f6190224570ff1ebafb3
Ashley Awuor <110401820+ashley-aa511@users.noreply.github.com>
2024-11-06 06:25:51 +0300

    Flask Babel

--- 1-app.py
+++ 1-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-#0-app.py
+#1-app.py
 from flask import Flask, render_template
 from flask_babel import Babel
 
@@ -20,7 +20,7 @@ babel = Babel(app)
 
 @app.route('/')
 def home():
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
