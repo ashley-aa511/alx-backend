75cdadf1bd5fa999384c5525e4fb68e7d95290ce
ashley-aa511 <okeyoashley@gmail.com>
2024-11-06 11:48:24 +0300

    Determines the best match for our supported languages

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+#2-app.py
+
+#Imports modules important for the application to work
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+#Configuration class
+class Config:
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+#Apply configuration to your class app
+app.config.from_object(Config)
+
+#Initialize Babel
+babel = Babel(app)
+
+#Locale Selector Function
+@babel.localeselector
+def get_locale():
+    #This returns which language it decides to use
+    return request.accept_languages.bestmatch(app.config['LANGUAGES'])
+
+@app.route('/')
+def home():
+    return render_template('2-index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
